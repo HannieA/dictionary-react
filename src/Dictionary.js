e6fd22889ea64e7c7dca0b392cccffdf9908a4e8
HannieA <ann.986@gmail.com>
2022-09-22 12:57:49 -0700

    Add axios and api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
+
+import "./Dictionary.css";
 
 export default function Dictionary() {
   const [value, setValue] = useState("");
 
   function search(event) {
     event.preventDefault();
-    alert(`${value}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`;
+    axios.get(apiUrl).then(showResponse);
   }
 
   function handleValue(event) {
     setValue(event.target.value);
+  }
+  function showResponse(response) {
+    console.log("my Logs");
+    console.log(response.data[0]);
   }
 
   return (
