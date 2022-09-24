69ae3d5e477bdda8f587db6e86717697aab9647f
HannieA <ann.986@gmail.com>
2022-09-23 20:35:10 -0700

    Add audio icon to listen phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileAudio } from "@fortawesome/free-regular-svg-icons";
 
 export default function Phonetics(props) {
   const usPhonetics = props.phonetics.filter((item) =>
@@ -8,8 +10,8 @@ export default function Phonetics(props) {
   return (
     <div className="Phonetics">
       <p>{usPhonetics[0].text}</p>
-      <a href={usPhonetics[0].audio} target="_blank">
-        Listen
+      <a href={usPhonetics[0].audio} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faFileAudio} />
       </a>
     </div>
   );
