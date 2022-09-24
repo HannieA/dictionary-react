cc4e67abe0a1c77e33a49b10ff53b03db27103ab
HannieA <ann.986@gmail.com>
2022-09-23 19:21:14 -0700

    Add text and audio in phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,19 +1,16 @@
 import React from "react";
 
 export default function Phonetics(props) {
-  props.phonetics.map(function (us, index) {
-    console.log("My logs");
-    console.log(us.text);
+  const usPhonetics = props.phonetics.filter((item) =>
+    item.audio.includes("us.mp3")
+  );
 
-    return (
-      <div key={index} className="Phonetics">
-        <strong>{us.text}</strong>
-      </div>
-    );
-  });
+  return (
+    <div className="Phonetics">
+      <p>{usPhonetics[0].text}</p>
+      <a href={usPhonetics[0].audio} target="_blank">
+        Listen
+      </a>
+    </div>
+  );
 }
-//   const usPhonetics = props.phonetics.filter((item) =>
-//     item.audio.includes("us.mp3")
-//   );
-
-//   return <div>{usPhonetics}</div>;
