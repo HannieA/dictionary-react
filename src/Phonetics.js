236d223592cbd353a4f7cba37253495e7d09120c
HannieA <ann.986@gmail.com>
2022-09-27 15:19:45 -0700

    Update phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -9,17 +9,16 @@ export default function Phonetics(props) {
   // const usPhonetics = props.phonetics.filter((item) =>
   //   item.audio.includes("us.mp3")
   // );
-  if (props.phonetics[0].audio) {
-    return (
-      <div className="Phonetics">
-        <p>
-          {" "}
-          <a href={props.phonetics[0].audio} target="_blank" rel="noreferrer">
-            {/* <FontAwesomeIcon icon={faFileAudio} /> */} Listen
-          </a>
-          <span className="text">{props.phonetics[0].text}</span>
-        </p>
-      </div>
-    );
-  } else return null;
+
+  return (
+    <div className="Phonetics">
+      <p>
+        {" "}
+        <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
+          {/* <FontAwesomeIcon icon={faFileAudio} /> */} Listen
+        </a>
+        <span className="text">{props.phonetics.text}</span>
+      </p>
+    </div>
+  );
 }
--- src/Results.js
+++ src/Results.js
@@ -5,13 +5,20 @@ import Meanings from "./Meanings";
 import Phonetics from "./Phonetics";
 
 export default function Results(props) {
+  console.log(props.results.phonetics);
   if (props.results) {
     return (
       <div className="Results">
         <section>
           <h2>{props.results.word}</h2>
 
-          <Phonetics phonetics={props.results.phonetics} />
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetics phonetics={phonetic} />
+              </div>
+            );
+          })}
         </section>
 
         {props.results.meanings.map(function (meaning, index) {
